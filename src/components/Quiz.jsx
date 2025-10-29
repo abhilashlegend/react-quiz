00210c5b2547dae5648884a28ec4f5f2e495c789
abhilashlegend <abhilash456a@live.com>
2025-10-29 13:49:45 +0530

    Added shuffled answers and completed screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import questions from "../questions";
+import TropyLogo from "../assets/quiz-complete.png";
 
 export default function Quiz() {
 
     
     const [userAnswers, setUserAnswers] = useState([]);
-    const activeQuestionIndex = userAnswers.length;
+   
+
+    const isQuizFinished = userAnswers.length === questions.length;
+
+    if (isQuizFinished) {
+        return (
+            <div id="summary">
+                <img src={TropyLogo} alt="Quiz Completed" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+    
+     const activeQuestionIndex = userAnswers.length;
+    const shuffledAnswers = questions[activeQuestionIndex].answers;
+        shuffledAnswers.sort(() => Math.random() - 0.5);
 
     function handleAnswerSelection(answer) {
         setUserAnswers(prevAnswers => {
@@ -17,7 +33,7 @@ export default function Quiz() {
         <div id="quiz">
             <h2 id="question"> { questions[activeQuestionIndex].text} </h2>
             <ul id="answers">
-                {questions[activeQuestionIndex].answers.map((answer, index) => (
+                {shuffledAnswers.map((answer, index) => (
                     <li key={index} className="answer">
                         <button onClick={() => handleAnswerSelection(answer)}>{ answer }</button>
                     </li>
